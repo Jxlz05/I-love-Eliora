e52c975b9846ad47f9bd1454cc03c4762aa8d517
Jxlz05 <julian.rittmann05@gmail.com>
2025-06-09 02:09:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function checkPassword() {
     if (password === correctPassword) {
         document.getElementById("login").style.display = "none";
         document.getElementById("message").style.display = "block";
-        var music = document.getElementById("wedding song");
+        var music = document.getElementById(" <audio id="Rachmaninoff - Piano Concerto 2 (Best Part)");
         music.play();
     } else {
         alert("Oopsies, try again sweetheart!");
